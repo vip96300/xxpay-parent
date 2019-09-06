ec4b1335a923d1ae8cd32f9bcf10b0eeae6da06c
vip96300@163.com <Hhf135797531>
2019-09-06 15:59:54 +0800

    commit message

--- spring-cloud/xxpay-service/src/main/java/org/xxpay/service/controller/NotifyWxPayController.java
+++ spring-cloud/xxpay-service/src/main/java/org/xxpay/service/controller/NotifyWxPayController.java
@@ -114,7 +114,7 @@ public class NotifyWxPayController extends NotifyBasePay {
 
 		//校验结果是否成功
 		if (!PayConstant.RETURN_VALUE_SUCCESS.equalsIgnoreCase(params.getResultCode())
-				|| !PayConstant.RETURN_VALUE_SUCCESS.equalsIgnoreCase(params.getResultCode())) {
+				|| !PayConstant.RETURN_VALUE_SUCCESS.equalsIgnoreCase(params.getReturnCode())) {
 			_log.error("returnCode={},resultCode={},errCode={},errCodeDes={}", params.getReturnCode(), params.getResultCode(), params.getErrCode(), params.getErrCodeDes());
 			payContext.put("retMsg", "notify data failed");
 			return false;
